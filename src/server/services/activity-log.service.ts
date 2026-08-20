defb42f9935bfde2eb57fe67ec60a3ffd32fa2c1
Nouran Elsayed <153534623+NouranElsyed@users.noreply.github.com>
2026-08-20 11:13:36 +0300

    Add new entities to logActivity function

--- src/server/services/activity-log.service.ts
+++ src/server/services/activity-log.service.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/server/db/client";
 export async function logActivity(params: {
   userId: string;
   action: "CREATE" | "UPDATE" | "DELETE" | "LOGIN" | "ACTIVATE" | "DEACTIVATE" | "RESET_PASSWORD";
-  entity: "PROJECT" | "CUSTOMER" | "DOCUMENT" | "USER" | "SETTINGS" | "TAKEOFF_DRAWING" | "TAKEOFF_PART" | "SCOPE_ITEM" | "NOTICE" | "MEETING_MINUTE";
+  entity: "PROJECT" | "CUSTOMER" | "DOCUMENT" | "USER" | "SETTINGS" | "TAKEOFF_DRAWING" | "TAKEOFF_PART" | "SCOPE_ITEM" | "NOTICE" | "MEETING_MINUTE" | "PART_DXF" | "NESTING_JOB" | "NESTING_JOB_ITEM";
   entityId?: string;
   detail?: string;
 }) {
